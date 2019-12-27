38e6f6c1d398ad0d6bf6da5a92c7ce4db86daf65
uptlALFD <52241137+uptlALFD@users.noreply.github.com>
2019-12-26 20:15:32 -0500

    Envio de Mensaje 1.1
    Nuevas Caracteristicas
    - ya no se limita a escribir solo un mensaje 
    - se elije entre dos mensaje a enviar aleatoriamente

--- ingswoct2019/IngSWoct2019.java
+++ ingswoct2019/IngSWoct2019.java
@@ -1,23 +1,35 @@
 package ingswoct2019;
 
+import java.util.Random;
+
 public class IngSWoct2019 {
     private static String sms;
     
     public static void enviaMensaje(String sms){
-        IngSWoct20191.sms = sms;
+        IngSWoct2019.sms = sms;
     }
     
     public static void recibirMensaje(){
-        System.out.println("Mensaje:"+ IngSWoct20191.sms);
+        System.out.println("Mensaje:"+ IngSWoct2019.sms);
     }
     
     public static void main(String[] args) {
+        
+        Random v =new Random();
+        int op;
+        String sms="";
+        
+        op = v.nextInt(3);
      
-        String sms= "Te estoy enviando un mensaje";
+        if(op==2){
+            sms= "Te estoy llamando a tu telefono, por favor atiende";
+        }else{
+            sms= "Te estoy enviando un mensaje";
+        }
         
-        IngSWoct20191.enviaMensaje(sms);
+        IngSWoct2019.enviaMensaje(sms);
         
-        IngSWoct20191.recibirMensaje();
+        IngSWoct2019.recibirMensaje();
         
         
         
